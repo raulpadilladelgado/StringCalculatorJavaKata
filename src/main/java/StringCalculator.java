c9c68587785e01ff862100cb63acd4551c363654
raulpadilladelgado <rpd27@outlook.com>
2020-05-15 19:39:14 +0100

    add support for different separators

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,21 +1,38 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
-    public static int add(String s) {
+    public static String add(String s) {
         if (s.isEmpty()) {
-            return 0;
+            return "0";
         }
-        if (!s.contains(",")) {
-            return parseInt(s);
+        if (!s.contains(",")&&!s.contains("\n")&&!s.contains("//")) {
+            return s;
         }
-        
-        List<String> numbers = Arrays.asList(s.split(","));
+
+        List<String> numbers;
+        if(s.contains("//")){
+            char separator = s.charAt(2);
+            s = s.substring(3);
+            numbers = separateString(s,separator);
+        }else{
+            numbers = separateString(s);
+        }
         int result = 0;
         result = sum_strings(numbers, result);
-        return result;
+        return String.valueOf(result);
+
+    }
+
+    private static List<String> separateString(String s) {
+        return Arrays.asList(s.split(",|\\n"));
+    }
+    private static List<String> separateString(String s,char separator) {
+        String regex = ",|\\n|".concat(String.valueOf(separator));
+        return Arrays.asList(s.split(regex));
     }
 
     private static int sum_strings(List<String> numbers, int result) {
